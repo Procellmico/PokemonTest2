299bfbd1233cd0de1b118cac16945ce601d2c679
procellmico <Procellmico@users.noreply.github.com>
2014-05-28 07:35:51 +0800

    Initial commit

---
+++ desktop/src/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import main.PokemonTest2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PokemonTest2(), config);
+	}
+}
