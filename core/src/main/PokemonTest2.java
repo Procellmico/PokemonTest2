46573ede0264a718ac3c2ba6f1beaeab78efa319
procellmico <Procellmico@users.noreply.github.com>
2014-05-28 21:11:57 +0800

    Overrid some methods for art loading and screen clearing

--- core/src/main/PokemonTest2.java
+++ core/src/main/PokemonTest2.java
@@ -1,12 +1,38 @@
 package main;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class PokemonTest2 extends Game
 {
 	@Override
 	public void create()
 	{
+		Art.loadArt();
+
 		setScreen(new GameScreen());
+	}
+
+	@Override
+	public void render()
+	{
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+
+		if (getScreen() != null)
+		{
+			getScreen().render(Gdx.graphics.getDeltaTime());
+		}
+	}
+
+	@Override
+	public void dispose()
+	{
+		if (getScreen() != null)
+		{
+			getScreen().hide();
+		}
+		Art.disposeArt();
 	}
 }
