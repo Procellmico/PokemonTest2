b82c7304442a14506efe5d53ef3a0eb6e264d738
procellmico <Procellmico@users.noreply.github.com>
2014-05-28 20:29:16 +0800

    Made Player Disposable

--- core/src/main/Player.java
+++ core/src/main/Player.java
@@ -1,13 +1,25 @@
 package main;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
 
-public class Player
+public class Player implements Disposable
 {
 	private Vector2 position;
 
 	public Player(float x, float y)
 	{
 		position = new Vector2(x, y);
+	}
+
+	public void onKeyTap(int keycode)
+	{
+
+	}
+
+	@Override
+	public void dispose()
+	{
+
 	}
 }
