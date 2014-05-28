43a267596bfbb4c89d0855d68af69594eb6d181a
procellmico <Procellmico@users.noreply.github.com>
2014-05-28 20:18:17 +0800

    Created Player class and added position

---
+++ core/src/main/Player.java
@@ -0,0 +1,13 @@
+package main;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Player
+{
+	private Vector2 position;
+
+	public Player(float x, float y)
+	{
+		position = new Vector2(x, y);
+	}
+}
