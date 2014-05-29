245302bdc3d1d6faf3a337efc91c264652b22ad4
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 19:43:50 +0800

    Added State stuff in Player

--- core/src/main/Player.java
+++ core/src/main/Player.java
@@ -11,6 +11,13 @@ public class Player implements Disposable
 	private PlayerGraphics graphics;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
+	private int currentState = -1;
+	private Direction currentDirection;
+	private float animationCounter = 0f;
+
+	private static final int IDLE_STATE = 0;
+	private static final int WALKING_STATE = 1;
+	private static final int RUNNING_STATE = 2;
 
 	public Player(float x, float y, OrthographicCamera cam)
 	{
@@ -18,10 +25,24 @@ public class Player implements Disposable
 		graphics = new PlayerGraphics(1f / 6f);
 		batch = new SpriteBatch();
 		camera = cam;
+		currentState = IDLE_STATE;
+		currentDirection = Direction.SOUTH;
 	}
 
 	public void render(float delta)
 	{
+		animationCounter += delta;
+
+		switch(currentState)
+		{
+		case IDLE_STATE:
+			graphics.currentTexture = graphics.idle[currentDirection.ordinal()];
+			break;
+		case WALKING_STATE:
+			graphics.currentTexture = graphics.walkingAnimation[currentDirection.ordinal()].getKeyFrame(animationCounter, true);
+			break;
+		}
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(graphics.currentTexture, position.x, position.y, 1f, (float)graphics.currentTexture.getRegionHeight() / (float)graphics.currentTexture.getRegionWidth());
