a5405b71ee97bf9f080e80c1cb4b9b8f7bdde69a
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 21:36:30 +0800

    Added speed to Global class

--- core/src/main/Global.java
+++ core/src/main/Global.java
@@ -5,4 +5,5 @@ public class Global
 	public static final float UNIT_SCALE = 1f / 32f;
 	public static final float TILES_IN_WIDTH = 20f;
 	public static final float HOLD_THRESHOLD = 0.15f;
+	public static final float SPEED = 0.05F;
 }
