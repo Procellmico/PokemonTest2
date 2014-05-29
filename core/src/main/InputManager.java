bf7048f211a317485042b3a189cec0dc12366506
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 20:55:03 +0800

    Overhauled the InputManager

--- core/src/main/InputManager.java
+++ core/src/main/InputManager.java
@@ -1,43 +1,50 @@
 package main;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 
 public class InputManager extends InputAdapter
 {
-	private boolean[] keyPressed;
-	private static InputManager instance;
-	private static Player player;
+	private Player player;
+	private boolean isKeyPressed[];
+	private float timePressed[];
 
-	private InputManager()
+	public InputManager(Player play)
 	{
-		keyPressed = new boolean[256];
+		player = play;
+		isKeyPressed = new boolean[256];
+		timePressed = new float[256];
+	}
+
+	public void update()
+	{
+		for (int i = 0; i < 256; i++)
+		{
+			if (isKeyPressed[i])
+			{
+				if (timePressed[i] > Global.HOLD_THRESHOLD)
+				{
+					player.onKeyPressed(i);
+				}
+				else
+				{
+					player.onKeyTapped(i);
+				}
+			}
+		}
 	}
 
 	public boolean keyDown(int keycode)
 	{
-		player.onKeyTap(keycode);
-		keyPressed[keycode] = true;
+		timePressed[keycode] += Gdx.graphics.getDeltaTime();
+		isKeyPressed[keycode] = true;
 		return true;
 	}
 
 	public boolean keyUp(int keycode)
 	{
-		keyPressed[keycode] = false;
+		timePressed[keycode] = 0f;
+		isKeyPressed[keycode] = false;
 		return true;
-	}
-
-	public static InputManager getInstance(Player player1)
-	{
-		if (instance == null)
-		{
-			instance = new InputManager();
-		}
-
-		if (player == null)
-		{
-			player = player1;
-		}
-
-		return instance;
 	}
 }
