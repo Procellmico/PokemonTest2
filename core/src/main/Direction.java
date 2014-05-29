5bd8886ff8ec10539176a7cf0d7ec63612a9a494
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 19:44:49 +0800

    Made length a hardcoded value hue

--- core/src/main/Direction.java
+++ core/src/main/Direction.java
@@ -8,7 +8,7 @@ public class Direction
 	public static final Direction SOUTH = new Direction(1, 0f, -1f);
 	public static final Direction EAST = new Direction(2, 1f, 0f);
 	public static final Direction WEST = new Direction(3, -1f, 0f);
-	public static int length = 0; //dat hardcoded value though
+	public static final int length = 4; //dat hardcoded value though
 
 	private final Vector2 velocity;
 	private final int ID;
@@ -17,7 +17,6 @@ public class Direction
 	{
 		velocity = new Vector2(x, y);
 		ID = ID1;
-		length++;
 	}
 
 	public Vector2 getVelocity(float speed)
