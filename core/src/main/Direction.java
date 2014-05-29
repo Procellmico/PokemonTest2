02db51999c6ac60a11d37a0e4e335e3b0e3f7858
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 21:19:39 +0800

    Added a key in Direction

--- core/src/main/Direction.java
+++ core/src/main/Direction.java
@@ -4,19 +4,26 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Direction
 {
-	public static final Direction NORTH = new Direction(0, 0f, 1f);
-	public static final Direction SOUTH = new Direction(1, 0f, -1f);
-	public static final Direction EAST = new Direction(2, 1f, 0f);
-	public static final Direction WEST = new Direction(3, -1f, 0f);
+	public static final Direction NORTH = new Direction(0, 0f, 1f, KeyBinding.UP);
+	public static final Direction SOUTH = new Direction(1, 0f, -1f, KeyBinding.DOWN);
+	public static final Direction EAST = new Direction(2, 1f, 0f, KeyBinding.RIGHT);
+	public static final Direction WEST = new Direction(3, -1f, 0f, KeyBinding.LEFT);
 	public static final int length = 4; //dat hardcoded value though
 
 	private final Vector2 velocity;
 	private final int ID;
+	private final int key;
 
-	private Direction(int ID1, float x, float y)
+	private Direction(int ID1, float x, float y, int key1)
 	{
 		velocity = new Vector2(x, y);
 		ID = ID1;
+		key = key1;
+	}
+
+	public int getKey()
+	{
+		return key;
 	}
 
 	public Vector2 getVelocity(float speed)
