b94e94439ef1e1ca71efb76016712a625228f651
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 21:36:19 +0800

    Edited more Player stuff and made camera to follow it

--- core/src/main/World.java
+++ core/src/main/World.java
@@ -2,6 +2,7 @@ package main;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.Disposable;
@@ -22,7 +23,7 @@ public class World implements Disposable
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, Global.TILES_IN_WIDTH, Global.TILES_IN_WIDTH / 16f * 9f);
 
-		player = new Player(5, 5, camera);
+		player = new Player(5, 5, camera, (TiledMapTileLayer)map.getLayers().get("data"));
 
 		bgLayer = new int[]{0};
 		fgLayer = new int[]{map.getLayers().getCount() - 1};
@@ -30,6 +31,7 @@ public class World implements Disposable
 
 	public void render(float delta)
 	{
+		camera.position.set(player.getPosition(), 0f);
 		camera.update();
 		mapRenderer.setView(camera);
 		mapRenderer.render(bgLayer);
