b982a183a3a95af8dbf1f06a71c96bf254543ed8
procellmico <Procellmico@users.noreply.github.com>
2014-05-29 20:30:18 +0800

    Created KeyBinding class

---
+++ core/src/main/KeyBinding.java
@@ -0,0 +1,13 @@
+package main;
+
+import com.badlogic.gdx.Input;
+
+public class KeyBinding
+{
+	public static final int UP = Input.Keys.UP;
+	public static final int DOWN = Input.Keys.DOWN;
+	public static final int LEFT = Input.Keys.LEFT;
+	public static final int RIGHT = Input.Keys.RIGHT;
+	public static final int A_BUTTON = Input.Keys.Z;
+	public static final int B_BUTTON = Input.Keys.X;
+}
