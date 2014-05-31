c53a3fb5631506377f573a280b8a797e611588ba
procellmico <Procellmico@users.noreply.github.com>
2014-05-31 21:42:36 +0800

    Added running animation

--- core/src/main/Art.java
+++ core/src/main/Art.java
@@ -9,6 +9,10 @@ public class Art
 	public static Texture playerUpWalk;
 	public static Texture playerDownWalk;
 	public static Texture playerIdle;
+	public static Texture playerLeftRun;
+	public static Texture playerRightRun;
+	public static Texture playerUpRun;
+	public static Texture playerDownRun;
 
 	public static void loadArt()
 	{
@@ -17,6 +21,10 @@ public class Art
 		playerUpWalk = new Texture("playerUpWalk2.png");
 		playerLeftWalk = new Texture("playerLeftWalk2.png");
 		playerRightWalk = new Texture("playerRightWalk2.png");
+		playerLeftRun = new Texture("playerLeftRun2.png");
+		playerRightRun = new Texture("playerRightRun2.png");
+		playerUpRun = new Texture("playerUpRun2.png");
+		playerDownRun = new Texture("playerDownRun2.png");
 	}
 
 	public static void disposeArt()
@@ -26,5 +34,9 @@ public class Art
 		playerUpWalk.dispose();
 		playerLeftWalk.dispose();
 		playerRightWalk.dispose();
+		playerUpRun.dispose();
+		playerLeftRun.dispose();
+		playerRightRun.dispose();
+		playerDownRun.dispose();
 	}
 }
